455c2e7e684559a6fbfbe49f6ed76570f6af2156
Santiago Posse <smposse@gmail.com>
2022-06-27 18:15:20 -0300

    Subiendo entrega final

--- js/clases.js
+++ js/clases.js
@@ -245,7 +245,6 @@ console.log(persona3.nombre);
 console.log(persona3);
 
 persona1.hablar();
-*/
 // OBJETO MATH
 
 const PI = Math.PI;
@@ -329,3 +328,227 @@ btnEnviar.onclick = () => {
 inputNombre.onkeyup = () => {
   divContenedor.innerText = inputNombre.value;
 };
+*/
+
+function asignarOperacion(op) {
+  if (op == "sumar") {
+    return (a, b) => a + b;
+  } else if (op == "restar") {
+    return (a, b) => a - b;
+  }
+}
+
+let suma = asignarOperacion("sumar");
+let resta = asignarOperacion("restar");
+
+console.log(suma(4, 6));
+console.log(resta(5, 3));
+console.log(suma(12, 40));
+
+const numeros = [1, 2, 3, 4, 5, 6];
+
+numeros.forEach((num) => {
+  console.log(num);
+});
+
+const miCompra = [
+  { nombre: "Desarrollo Web", precio: 20000 },
+  { nombre: "Javascript", precio: 18750 },
+  { nombre: "ReactJS", precio: 27500 },
+];
+
+const total = miCompra.reduce(
+  (precioAcumulado, elemento) => precioAcumulado + elemento.precio,
+  0
+);
+console.log(total);
+
+const nombre = localStorage.setItem("nombre", "Manuel");
+
+const valorNombre = localStorage.getItem("nombre");
+
+for (let i = 0; i > 10; i++) {}
+let num = 10;
+num = num + 1;
+//Sugar Syntax
+num += 1;
+num++;
+// Operador ternario ? simplifica el if...else    ofrece un return por defecto
+// Condición ? caso1 : caso2
+
+let temperatura = 31;
+if (temperatura > 30) {
+  alert("Día caluroso");
+} else {
+  alert("Día agradable");
+}
+
+//Sugar Syntax
+temperatura > 30 ? alert("Día caluroso") : alert("Día agradable");
+
+//Operador && para reducir un if sencillo también - Hace una devolución si la condición es verdadera. Si no, no devuleve nada.
+
+//Operador lógico or ||
+
+//Operador Nulish Coal   ??   ... - Funciona como el or pero admite más de un valor verdadero. Permite acceder a los objetos y no permitir que nos devulva un error en la ejecución. Se debe agregar el ? a objeto que se desea hacer la comparación por ej:
+// console.log (producto1?.nombre || "El usuario no existe");
+
+//Destructuración permite utilizar variables contenidas dentro de un objeto, por ejemplo   let {prop1, prop2} = objeto   - declaro las variables que quiero, que pertenecen al objeto
+
+//Spread - te permite comvertir el contenido de un array en un string  -  Se puede concatenar arrays y/o aobjetos - Se puede copiar un objeto y cambiarle alguna de las variables.
+//Spred también se puede pasar como parámetro rest
+
+function sumar(...numeros) {
+  console.log(/*hacer un for o algo para que tome la cantidad exacta de arámetros*/);
+}
+
+sumar(4, 6, 98, 242);
+
+//Podría usarse el spreed operator para sumar los precios del carrito por ejemplo
+//Existen distintas librerías Java Script - si instalamos con CDN, se debe poner el script antes de nuestros propios scripts
+//Ver Sweetalert y Toastify (para producto cargado al carrito)
+//Luxon es una librería que permite trabajar con fechas
+// Cleave js permite dar formato por ejemplo a los números de la tarjeta de crédito
+
+// Clase 8 - Modelos de Programación sincrónica y asincrónica
+// Settimeout - es una función que nos permite realizar acciones asincrónicamente. Recibe 2 parámetros, un callback y un valor numérico.
+setTimeout(() => {
+  console.log("Proceso asincrónico");
+}, 3000);
+const btn = document.querySelector("#boton");
+const popup = document.querySelector("#popup-mensaje");
+
+btn.addEventListener("click", () => {
+  popup.classList.add("popup-active");
+
+  setTimeout(() => {
+    popup.classList.remove("popup-active");
+  }, 2500);
+});
+
+// Call stack (Pila) - Es una lista de tareas donde se van acumulando las tareas que nosotros le pedimos a Js, que haga.
+// Event Loop - Muchas funciones asincrónicas se ejecutan en un stack diferente. El Event Loop es la herramienta que permite la sincronización entre nuestro call stack con estas tareas asincrónicas que funcionan en un thread aparte. Si el stack está vacío, el Event Loop envía la primera función que esté en la callback queue al call stack y comienza a ejecutarse
+// Set Interal - similar a SetTimeout, pero nos permite repetir una ejecución cada determinada cantidad de tiempo, por ej imprimir un log cada 1 seg.
+setInterval(() => {
+  console.log("Tic");
+}, 1000);
+// Se repite indefinidamente a menos que lo paremos.
+// Clearinterval y Cleartimeout, para removerlos
+
+let contador = 0;
+let intervalo = setInterval(() => {
+  contador++;
+  console.log(contador);
+  if (contador >= 20) {
+    clearInterval(intervalo);
+  }
+}, 1000 /*podría ser una variable o un dato que ingrese el usuario o cualquier cosa*/);
+
+// PROMESAS - Es un objeto que representa un evento futuro. Una promesa cuenta con 3 estados posibles. Pending / Fulfilled / Rejected. Las promesas pueden ser resultas o rechazadas.
+// new Promise recibe una función como parámetro, que a su vez revise por parámetro resolve y reject.
+new Promise((resolve, reject) => {
+  //cuerpo de la promesa
+});
+
+const eventoFuturo = () => {
+  return new Promise((resolve, reject /*resolve y reject son funciones*/) => {
+    //código a ejecutar
+    console.log("Hola tarola");
+  });
+};
+console.log(eventoFuturo());
+
+/*El valor de retorno de la promesa se define a través del llamado a las funciones de resolve o reject:
+Si el cuerpo de la promesa llama a resolve(), la promesa cambiará su estado a fulfilled, con el valor enviado a resolve().
+Si la promesa llama a reject(), cambiará su estado a rejected con el valor enviado al reject().
+*/
+const eventoFuturo2 = (res) => {
+  return new Promise((resolve, reject) => {
+    if (res === true) {
+      resolve("Promesa resuelta");
+    } else {
+      reject("Promesa rechazada");
+    }
+  });
+};
+
+console.log(eventoFuturo(true)); // Promise { 'Promesa resuelta' }
+console.log(eventoFuturo(false)); // Promise { <rejected> 'Promesa rechazada' }
+
+const eventoFuturo3 = (res) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      res ? resolve("Promesa resuelta") : reject("Promesa rechazada");
+    }, 2000);
+  });
+};
+
+console.log(eventoFuturo(true)); // Promise { <pending> }
+console.log(eventoFuturo(false)); // Promise { <pending> }
+
+// Hay 2 métodos más para trabajar con las promesas que son Then y Catch
+/*Al llamado de una función que retorne una promesa, podemos concatenar el método .then() o .catch(), los cuales reciben una función por parámetro con la cual se captura el valor de la promesa:
+.then() : Si la promesa es resuelta ✅, su valor de retorno se captura dentro del .then(), recibiendo por parámetro de su función ese valor.
+.catch() : si la promesa es rechazada ❌, su valor se captura dentro de un .catch() siguiendo la misma lógica.*/
+
+//Lo que queramos ejecutar cuando la promesa se resuelva o rechace, debemos definirlo dentro de un .then() o .catch(), según el caso 🙌:
+eventoFuturo(true).then((response) => {
+  console.log(response); // Promesa resuelta
+});
+
+eventoFuturo(false).catch((error) => {
+  console.log(error); // Promesa rechazada
+});
+// Se puede anidar
+console.log(
+  eventoFuturo()
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error); // Promesa rechazada
+    })
+);
+
+// Finally - finally() es un método que recibe una función la cual se ejecutará siempre al finalizar la secuencia, sin importar si se haya resuelto o no la promesa
+// Se ejecuta aún si la respuesta es exitosa o rechazada. También se lo puede anidar o concatenar.
+eventoFuturo(true)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Fin del proceso");
+  });
+// Promesa resuelta
+// Fin del proceso
+
+// Ajax - AJAX (JavaScript Asíncrono y XML), un conjunto de técnicas de desarrollo que permiten que las aplicaciones web funcionen de forma asincrónica para procesar tareas en segundo plano
+/*Javascript nos ofrece el método fetch() para hacer peticiones HTTP a algún servicio externo. Como estas peticiones son asincrónicas, convenientemente el método fetch() trabaja con promesas.
+El método recibe un primer parámetro que es la URL a la cual hacer la petición, y un segundo parámetro opcional de configuración:*/
+console.log(fetch("https://jsonplaceholder.typicode.com/posts"));
+// Promise {<pending>}
+//Retorna una Promesa pendiente. Para trabajar con la resolución de la petición, debemos hacerlo dentro del .then() correspondiente:
+fetch("https://jsonplaceholder.typicode.com/posts").then((resp) =>
+  console.log(resp)
+);
+
+// Hay que crear un archivo json, se puede crear en la carpeta Js. Puede ser data.json
+// En los json las llaves y los valores van todos con "", a menos que sea número.
+// Le pasamos al fetch la ruta de nuestro archivo json. Es una ruta relativa a mi proyecto.
+// Podría tener el array de productos en un archivo json y traerlo con un fetch.
+
+// Async y await - La sentencia await nos permite establecer un punto de espera en el código. Aplicado como prefijo a una promesa (en este caso, el return del fetch) se bloquea la ejecución de la siguiente instrucción hasta que la promesa se resuelva
+// Pero await sólo puede utilizarse dentro de una función asincrónica 😕. Aquí es donde entra la sentencia async. Ésta palabra reservada sirve para declarar una función como asincrónica, y se agrega como prefijo a la función
+// Al usar el await, tal vez ya no sea necesario poner el .then
+// Ver clip js
+// Tiene que haber un fetch aunque sea consumiendo un json interno
+
+// Poner propiedad amount y también total price
+
+// Debería pasarle un precio para cada tamaño seleccionado
+
+// Para empezar React, repasar objetos, arrays, iteradores para recorrerlos(ver map, filter), fetch y apis externas, spread operator, destructury
+// El simulador debe terminar el proceso, no solo sumar al carrito de compras, sino también finalizar "simular" la compra
--- js/main.js
+++ js/main.js
@@ -1,105 +1,301 @@
-/*Sumar de 3 en 3 - se me hace bucle infinito, no entiendo por qué
-let n1 = parseInt(prompt("Ingrese un número"));
-let n2 = parseInt(prompt("Ingresar otro número"));
-let resultado = n1 + n2;
-while (n1 != "ESC") {
-  console.log(resultado);
-  n1 = prompt("Ingresar otro número");
+// Get element
+const button_logIn = document.getElementById("button_login");
+const ingresoNombreYapellido = document.getElementById("inputNombre");
+const ingresoMail = document.getElementById("inputMail");
+const ingresoMesa = document.getElementById("inputMesa");
+const formularioInicial = document.querySelector(".inicial_form");
+const mensajeInicial = document.getElementById("mensaje_inicial");
+const totalTotal = document.getElementById("itemCartTotal");
+const contadorCarrito = document.getElementById("cart-counter");
+const buttonPagar = document.getElementById("button_pagar");
+buttonPagar.addEventListener("click", pagar);
+button_logIn.addEventListener("click", guardarLogIn);
+
+//Login - Local Storage
+function guardarLogIn() {
+  if (
+    !ingresoNombreYapellido.value ||
+    !ingresoMail.value ||
+    !ingresoMesa.value ||
+    ingresoMesa.value < 1 ||
+    ingresoMesa.value > 20
+  ) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Debe ingresar Nombre y Apellido, Mail y un número de mesa válido (entre 1 y 20)",
+    });
+  } else {
+    localStorage.setItem(
+      "Nombre",
+      JSON.stringify(ingresoNombreYapellido.value)
+    );
+    localStorage.setItem("Mesa", JSON.stringify(ingresoMesa.value));
+    mensajeInicial.innerHTML = "";
+    mensajeInicial.innerHTML = `<h2>Usted va a hacer un pedido a nombre de ${localStorage.getItem(
+      "Nombre"
+    )}</h2>
+    <h3>para la mesa N° ${localStorage.getItem("Mesa")}</h3>
+    </div>
+  `;
+    button_logIn.innerText = "Modificar";
+  }
 }
-let ingresarNumero = parseInt(prompt("Ingresar Numero"));
-for (let i = 1; i <= 100; i = i + 3) {
-  let resultado = ingresarNumero + i;
-  alert(ingresarNumero + " + " + i + " = " + resultado);
-}
-*/
-const saludar = () => {
-  let nombre = prompt("Ingrese su nombre");
-  let apellido = prompt("Ingrese su apellido");
-  while (!nombre || !apellido) {
-    nombre = prompt("Ingrese su nombre");
-    apellido = prompt("Ingrese su apellido");
-  }
-  return nombre + " " + apellido;
-};
-
-let nombreUsuario = saludar();
-alert("Hola " + nombreUsuario);
-
-class Pedido {
-  constructor(saborPizza, tamañoPizza, tamañoPapas, colorBirra) {
-    this.saborPizza = saborPizza;
-    this.tamañoPizza = tamañoPizza;
-    this.tamañoPapas = tamañoPapas;
-    this.colorBirra = colorBirra;
+function chequeoLoginEnStorage() {
+  if (JSON.parse(localStorage.getItem("Nombre"))) {
+    mensajeInicial.innerHTML = "";
+    mensajeInicial.innerHTML = `<h2>Usted va a hacer un pedido a nombre de ${localStorage.getItem(
+      "Nombre"
+    )}</h2>
+    <h3>para la mesa N° ${localStorage.getItem("Mesa")}</h3>
+    </div>
+  `;
+    button_logIn.innerText = "Modificar";
   }
 }
-const Carrito = [];
+chequeoLoginEnStorage();
 
-const crearPedido = () => {
-  let saborPizza = prompt(
-    "Qué sabor de Pizza querés:\n 1) Muzarella\n 2) Fugazeta\n 3) Roquefort"
-  );
-  while (
-    saborPizza !== "Muzarella" &&
-    saborPizza !== "Fugazeta" &&
-    saborPizza !== "Roquefort"
-  ) {
-    saborPizza = prompt(
-      "Qué sabor de Pizza querés:\n 1) Muzzarella\n 2) Fugazeta\n 3) Roquefort"
-    );
+// Función Contructora
+class Producto {
+  constructor(nombre, tamaño, precio, id, img, cantidad) {
+    this.nombre = nombre;
+    this.tamaño = tamaño;
+    this.precio = precio;
+    this.id = id;
+    this.img = img;
+    this.cantidad = cantidad;
+    this.precioFinal = this.precio * this.cantidad;
   }
-  let tamañoPizza = prompt(
-    "Qué tamaño de Pizza querés:\n 1) Grande\n 2) Mediana\n 3) Chica"
-  );
-  while (
-    tamañoPizza !== "Grande" &&
-    tamañoPizza !== "Mediana" &&
-    tamañoPizza !== "Chica"
-  ) {
-    tamañoPizza = prompt(
-      "Qué tamaño de Pizza querés:\n 1) Grande\n 2) Mediana\n 3) Chica"
-    );
-  }
-  let tamañoPapas = prompt(
-    "Qué tamaño de Papas querés:\n 1) Grandes\n 2) Medianas\n 3) Chicas"
-  );
-  while (
-    tamañoPapas !== "Grandes" &&
-    tamañoPapas !== "Medianas" &&
-    tamañoPapas !== "Chicas"
-  ) {
-    tamañoPapas = prompt(
-      "Qué tamaño de Papas querés:\n 1) Grandes\n 2) Medianas\n 3) Chicas"
-    );
-  }
-  let colorBirra = prompt(
-    "Qué color de birra querés:\n 1) Rubia\n 2) Negra\n 3) Roja"
-  );
-  while (
-    colorBirra !== "Rubia" &&
-    colorBirra !== "Negra" &&
-    colorBirra !== "Roja"
-  ) {
-    colorBirra = prompt(
-      "Qué color de birra querés:\n 1) Rubia\n 2) Negra\n 3) Roja"
-    );
-  }
-  let pedidoNuevo = new Pedido(
-    saborPizza,
-    tamañoPizza,
-    tamañoPapas,
-    colorBirra
-  );
-  Carrito.push(pedidoNuevo);
-  let seguir = confirm("Desea realizar otro pedido?");
-  if (seguir == true) {
-    pedidoNuevo = crearPedido();
-  }
-};
+}
+// Fetch
+fetch("./datos.json")
+  .then((resp) => resp.json())
+  .then((data) => crearCards(data));
+const cards_section = document.getElementById("cards_section");
+const carrito_section = document.getElementById("carrito_section");
+const total_carrito = document.getElementById("totalCarrito");
 
-let pedidoNuevo = crearPedido();
-console.log(Carrito);
+// Creo cards de cada objeto dentro del array que traigo por fetch
+function crearCards(array) {
+  array.forEach((producto) => {
+    cards_section.innerHTML += `<div class ="card col-lg-2 col-md-3 col-sm-4 m-1">
+    <img src=${producto.img} class= "card-image-top" alt="...">
+    <h5 class= "card-title text-center">${producto.nombre}</h5>
+    <div class= "card-body text-center">
+  
+    <p class= "card-text">${producto.precio}</p>
+    <button id="btn${producto.id}">Añadir al carrito<i class="fa fa-shopping-cart in-card" aria-hidden="true"></i> </button>
+  </div>`;
+  });
+  array.forEach((producto) => {
+    document
+      .querySelector(`#btn${producto.id}`)
+      .addEventListener("click", (e) => {
+        const productoCLickeado = array.find(
+          (item) => `btn${item.id}` == e.target.id
+        );
 
-//Método Find para saber si pidieron Pizza de Roquefort
-const hayRoquefort = Carrito.find((el) => el.saborPizza === "Roquefort");
-console.log(hayRoquefort);
+        console.log(productoCLickeado);
+        enviarAlCarrito(productoCLickeado);
+        mostrarCarrito();
+      });
+  });
+}
+// Carrito
+
+let Carrito = JSON.parse(localStorage.getItem("Carrito")) || [];
+mostrarCarrito();
+
+function enviarAlCarrito(productoCLickeado) {
+  if (
+    !JSON.parse(localStorage.getItem("Nombre")) ||
+    !JSON.parse(localStorage.getItem("Mesa"))
+  ) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Debe ingresar primero Nombre, Mail y Mesa",
+    });
+  } else {
+    let existe = Carrito.some((element) => element.id === productoCLickeado.id);
+    if (!existe) {
+      productoCLickeado.cantidad = 1;
+      Carrito.push(productoCLickeado);
+    } else {
+      productoCLickeado.cantidad++;
+    }
+    Toastify({
+      text: "Producto agregado al carrito",
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #ada5a5, #948b8b)",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+  }
+}
+function mostrarCarrito() {
+  carrito_section.innerHTML = "";
+  Carrito.forEach((producto) => {
+    carrito_section.innerHTML += `<div class ="card">
+    <img src=${producto.img} class= "card-image-top" alt="...">
+    <h5 class= "card-title text-center">${producto.nombre}</h5>
+    <div class= "card-body text-center">
+  
+    <p class= "card-text">${producto.tamaño}</p>
+    <p id="precioPorcantidad${producto.id}" class= "card-text">$${
+      producto.precio * producto.cantidad
+    }</p>
+    <button class= "btn btn-dark" id="menos_carrito${
+      producto.id
+    }"> <i class="fa fa-minus" aria-hidden="true"></i> </button> <span id="cant_counterCarrito${
+      producto.id
+    }" class ="item-count rounded ms-3 me-3">${
+      producto.cantidad
+    }</span><button class= "btn btn-dark" id="mas_carrito${
+      producto.id
+    }"><i class="fa fa-plus rounded" aria-hidden="true"></i> </button><br><br>
+    <button class= "btn btn-danger" id="borrar${
+      producto.id
+    }">Eliminar Todos<i class="fa fa-shopping-cart in-card" aria-hidden="true"></i> </button>
+</div>`;
+  });
+  Carrito.forEach((producto) => {
+    document
+      .querySelector(`#menos_carrito${producto.id}`)
+      .addEventListener("click", () => {
+        if (`${producto.cantidad}` < 2) {
+          Carrito = Carrito.filter((element) => element.id !== producto.id);
+          mostrarCarrito();
+          Toastify({
+            text: "Producto eliminado",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #F299A0, #F26B76)",
+            },
+            onClick: function () {}, // Callback after click
+          }).showToast();
+        } else {
+          `${producto.cantidad--}`;
+          document.querySelector(
+            `#cant_counterCarrito${producto.id}`
+          ).innerText = `${producto.cantidad}`;
+          document.querySelector(
+            `#precioPorcantidad${producto.id}`
+          ).innerText = `$${producto.precio * producto.cantidad}`;
+          sumarTotal();
+        }
+      });
+  });
+  Carrito.forEach((producto) => {
+    document
+      .querySelector(`#mas_carrito${producto.id}`)
+      .addEventListener("click", () => {
+        `${producto.cantidad++}`;
+        document.querySelector(
+          `#cant_counterCarrito${producto.id}`
+        ).innerText = `${producto.cantidad}`;
+        document.querySelector(
+          `#precioPorcantidad${producto.id}`
+        ).innerText = `$${producto.precio * producto.cantidad}`;
+        sumarTotal();
+      });
+  });
+
+  localStorage.setItem("Carrito", JSON.stringify(Carrito));
+  borrarProducto();
+  sumarTotal();
+}
+function borrarProducto() {
+  Carrito.forEach((producto) => {
+    document
+      .querySelector(`#borrar${producto.id}`)
+      .addEventListener("click", () => {
+        Carrito = Carrito.filter((element) => element.id !== producto.id);
+        mostrarCarrito();
+        Toastify({
+          text: "Producto eliminado",
+          duration: 3000,
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "right", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: "linear-gradient(to right, #F299A0, #F26B76)",
+          },
+          onClick: function () {}, // Callback after click
+        }).showToast();
+      });
+  });
+}
+
+// Sumar Total
+function sumarTotal() {
+  let suma = Carrito.reduce((acc, item) => {
+    return (acc = acc + item.precio * item.cantidad);
+  }, 0);
+  totalTotal.innerHTML = `Total a pagar $${suma}`;
+  contadorCarrito.innerText = `$${suma}`;
+}
+// Pagar
+function pagar() {
+  if (JSON.parse(localStorage.getItem("Carrito")) == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "El Carrito no debe estar vacío",
+    });
+  } else {
+    Swal.fire({
+      title: "Pagar",
+      html: `<div class="col-md6 mb-3 mx-auto">
+      <label for="tarjeta de crédito" class="form-label">Tarjeta de crédito</label>
+      <input type="text" class="form-control" placeholder="Ingrese el número de su tarjeta de crédito/débito"/></div>
+      <div class="col-md6 mb-3 mx-auto">
+      <label for="DNI" class="form-label">DNI</label>
+      <input type="text" class="form-control" placeholder="Ingrese su número de documento"/></div>
+      <div class="col-md6 mb-3 mx-auto">
+      <label for="nombre" class="form-label">Nombre y apellido</label>
+      <input type="text" class="form-control" placeholder="Ingrese su nombre tal como figura en la tarjeta"/></div>
+      <div class="col-md6 mb-3 mx-auto">
+      <label for="Mes vencimiento" class="form-label">Mes de Vencimiento</label>
+      <input type="text" class="form-control" placeholder="Ingrese mes de vencimiento"/></div>
+      <div class="col-md6 mb-3 mx-auto">
+      <label for="Año vencimiento" class="form-label">Año vencimiento</label>
+      <input type="text" class="form-control" placeholder="Ingrese año de vencimiento"/></div>
+      <div class="col-md6 mb-3 mx-auto">
+      <label for="Código de seguridad" class="form-label">Código de seguridad</label>
+      <input type="password" class="form-control" placeholder="Ingrese el código de seguridad de su tarjeta"/></div>
+      `,
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Pagar",
+      denyButtonText: `Modificar`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire(
+          "Su compra fue procesada con éxito! En breve le llevarán su pedido a la mesa",
+          "",
+          "success"
+        );
+      } else if (result.isDenied) {
+        Swal.fire("Puede seguir comprando", "", "info");
+      }
+    });
+  }
+}
+
+mostrarCarrito();
+sumarTotal();
